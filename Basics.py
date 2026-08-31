711b24a47263f8129c69e9452d45e9398f962f89
FahdJ0 <2rechange@gmail.com>
2026-08-31 17:02:07 +0100

    Add advanced password login script

--- Basics.py
+++ Basics.py
@@ -1,9 +1,13 @@
-password = input("Enter a password: ")
+trys = 3
+password = input("Create your password: ")
 while len(password) < 8:
-    print("The password contains less than 8 caracters, rewrite one.")
-    password = input("Enter a longer password: ")
-
-cpassword = input("The password you wrote is valid, confirm it: ")
-while cpassword != password:
-    cpassword = input("The second password is the same as the first one, retry: ")
-print("You comfirmed your password right")  
+    password = input("The password must be minimum 8 caracters, retry: ")
+cpassword = input("Confirm your password: ")
+while password != cpassword:
+    cpassword = input("Those passwords are not the same, retry: ")
+login = input("Now you have to log-in,you have 3 try, use the password: ")
+while login != password and trys > 1:
+    trys = trys -1
+    login = input(f"To log-in, you have to enter the password you set, you have {trys} try left: ")
+    if trys == 0:
+        print("Too many errors, you can't try anymore.")
